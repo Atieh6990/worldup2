5c02f746d8e1193ed4ae9a90e81df56bfe58d812
zandAkbari <pouriarubiker@gmail.com>
2022-10-30 12:24:48 +0330

    changed name of token on tizen

--- src/worldcup/views/menuRout.vue
+++ src/worldcup/views/menuRout.vue
@@ -13,6 +13,7 @@
         <div class="title" style="font-size: 16px;margin-top: 8%"><b>{{ item.name }}</b></div>
         <div class="title" style="font-size: 12px;margin-top: 0%">{{ item.des }}</div>
       </div>
+
     </div>
 
   </div>
